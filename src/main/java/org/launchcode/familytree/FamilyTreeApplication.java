f4f74f22f693b69f2557665b60ed5094511dd936
Emily Spradling <spradling.ed@gmail.com>
2023-01-12 19:45:14 -0600

    Initial code push

---
+++ src/main/java/org/launchcode/familytree/FamilyTreeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.familytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/familytree/FamilyTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.familytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
