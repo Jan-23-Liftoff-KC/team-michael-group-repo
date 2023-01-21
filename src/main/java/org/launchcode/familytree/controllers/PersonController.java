9a9f40ff489b5e40de0539ae77d6a176347a28f8
Emily Spradling <spradling.ed@gmail.com>
2023-01-21 10:06:54 -0600

    added person controller

---
+++ src/main/java/org/launchcode/familytree/controllers/PersonController.java
@@ -0,0 +1,16 @@
+package org.launchcode.familytree.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="person")
+public class PersonController {
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "About Me");
+        return "person/index";
+    }
+}
