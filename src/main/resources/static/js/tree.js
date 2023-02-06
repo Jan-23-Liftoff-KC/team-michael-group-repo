e92463f8ed7109a587f1aa436a963b47529dc4aa
gabrielvalencia <gabev13@gmail.com>
2023-02-05 20:35:01 -0600

    data for tree is now external of tree.js

--- src/main/resources/static/js/tree.js
+++ src/main/resources/static/js/tree.js
@@ -7,184 +7,131 @@ let svg = d3
   .attr("height", height);
 let gContainer = svg.append("g").attr("transform", "translate(80,50)");
 
-// Data, as parsed from csv (change later)
-// Add ids!
-let data = [
-  {
-    child: "John",
-    parent: "",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-    spouse: "Isabella",
-  },
-  {
-    child: "Kevin",
-    parent: "John",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-    spouse: "Emma",
-  },
-  {
-    child: "Aaron",
-    parent: "Kevin",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-  {
-    child: "Hannah",
-    parent: "Ann",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-    spouse: "William",
-  },
-  {
-    child: "Rose",
-    parent: "Sarah",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-  {
-    child: "Ann",
-    parent: "John",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-    spouse: "George",
-  },
-  {
-    child: "Sarah",
-    parent: "Kevin",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-    spouse: "James",
-  },
-  {
-    child: "Mark",
-    parent: "Ann",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-  {
-    child: "Angel",
-    parent: "Sarah",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-  {
-    child: "Tom",
-    parent: "Hannah",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-  {
-    child: "Amy",
-    parent: "Hannah",
-    icon: "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true",
-  },
-];
+// Load data, currently a json file from github
+d3.json("https://raw.githubusercontent.com/gvalencia4/D3/main/Family%20Tree/test-data.json")
+  .then(function(data) {
+    // Code from your callback goes here
+    console.log(data);
 
-// Data
-let dataStructure = d3
-  .stratify()
-  .id(function (d) {
-    return d.child;
+    let dataStructure = d3
+      .stratify()
+      .id(function (d) {
+        return d.child;
+      })
+      .parentId(function (d) {
+        return d.parent;
+      })(data);
+
+    let treeStructure = d3
+      .tree()
+      .separation(function (a, b) {
+        return a.parent === b.parent ? 2 : 2;
+      })
+      .size([650, 300]);
+
+    let information = treeStructure(dataStructure);
+
+
+    // console.log(information.descendants());
+    // console.log(information.links());
+
+    // For quickly adjusting person cards in the x
+    let personCardLocation = 0;
+
+    var rectangleOutline = svg
+      .append("rect")
+      .attr("id", "toZoom")
+      .attr("width", width)
+      .attr("height", height);
+
+    // Elbow Connectors
+    let connections = gContainer
+      .append("g")
+      .selectAll("path")
+      .data(information.links());
+    connections
+      .enter()
+      .append("path")
+      .attr("d", function (d) {
+        return (
+          "M" +
+          (d.source.x - personCardLocation) +
+          "," +
+          d.source.y +
+          " v 50 H" +
+          d.target.x +
+          " V" +
+          d.target.y
+        );
+      });
+
+    // Rectangles will become Person cards (Name, age, picture, etc.)
+    let rectangles = gContainer
+      .append("g")
+      .selectAll("rect")
+      .data(information.descendants());
+    rectangles
+      .enter()
+      .append("rect")
+      .classed("shape", true)
+      .attr("x", function (d) {
+        return d.x - 60 - personCardLocation;
+      })
+      .attr("y", function (d) {
+        return d.y - 20;
+      }); //or y - x/3.236
+
+    // Spouses
+    // let spouseRectangles = gContainer.append("g").selectAll("rect").data(information.descendants());
+    // spouseRectangles.enter().append("rect")
+    //   .attr("x", function(d){return d.x + 60 - personCardLocation})
+    //   .attr("y", function(d){return d.y - 20}) //or y - x/3.236
+    //   .classed("hide", function (d) {
+    //     if(d.data.spouse == undefined)
+    //       return true;
+    //     else
+    //       return false;
+    //   });
+
+    // Pictures
+    let pictures = gContainer
+      .append("g")
+      .selectAll("image")
+      .data(information.descendants());
+    pictures
+      .enter()
+      .append("image")
+      .attr("href", function (d) {
+        return d.data.icon;
+      })
+      .attr("x", function (d) {
+        return d.x - 65 - personCardLocation;
+      })
+      .attr("y", function (d) {
+        return d.y - 17;
+      });
+
+    // Name text
+    let names = gContainer
+      .append("g")
+      .selectAll("text")
+      .data(information.descendants());
+    names
+      .enter()
+      .append("text")
+      .text(function (d) {
+        return d.data.child;
+      })
+      .attr("x", function (d) {
+        return d.x + 20 - personCardLocation;
+      })
+      .attr("y", function (d) {
+        return d.y + 5;
+      });
   })
-  .parentId(function (d) {
-    return d.parent;
-  })(data);
-
-let treeStructure = d3
-  .tree()
-  .separation(function (a, b) {
-    return a.parent === b.parent ? 2 : 2;
-  })
-  .size([650, 300]);
-
-let information = treeStructure(dataStructure);
-// console.log(information.descendants());
-// console.log(information.links());
-
-// For quickly adjusting person cards in the x
-let personCardLocation = 0;
-
-var rectangleOutline = svg
-  .append("rect")
-  .attr("id", "toZoom")
-  .attr("width", width)
-  .attr("height", height);
-
-// Elbow Connectors
-let connections = gContainer
-  .append("g")
-  .selectAll("path")
-  .data(information.links());
-connections
-  .enter()
-  .append("path")
-  .attr("d", function (d) {
-    return (
-      "M" +
-      (d.source.x - personCardLocation) +
-      "," +
-      d.source.y +
-      " v 50 H" +
-      d.target.x +
-      " V" +
-      d.target.y
-    );
-  });
-
-// Rectangles will become Person cards (Name, age, picture, etc.)
-let rectangles = gContainer
-  .append("g")
-  .selectAll("rect")
-  .data(information.descendants());
-rectangles
-  .enter()
-  .append("rect")
-  .classed("shape", true)
-  .attr("x", function (d) {
-    return d.x - 60 - personCardLocation;
-  })
-  .attr("y", function (d) {
-    return d.y - 20;
-  }); //or y - x/3.236
-
-// Spouses
-// let spouseRectangles = gContainer.append("g").selectAll("rect").data(information.descendants());
-// spouseRectangles.enter().append("rect")
-//   .attr("x", function(d){return d.x + 60 - personCardLocation})
-//   .attr("y", function(d){return d.y - 20}) //or y - x/3.236
-//   .classed("hide", function (d) {
-//     if(d.data.spouse == undefined)
-//       return true;
-//     else
-//       return false;
-//   });
-
-// Pictures
-let pictures = gContainer
-  .append("g")
-  .selectAll("image")
-  .data(information.descendants());
-pictures
-  .enter()
-  .append("image")
-  .attr("href", function (d) {
-    return d.data.icon;
-  })
-  .attr("x", function (d) {
-    return d.x - 65 - personCardLocation;
-  })
-  .attr("y", function (d) {
-    return d.y - 17;
-  });
-
-// Name text
-let names = gContainer
-  .append("g")
-  .selectAll("text")
-  .data(information.descendants());
-names
-  .enter()
-  .append("text")
-  .text(function (d) {
-    return d.data.child;
-  })
-  .attr("x", function (d) {
-    return d.x + 20 - personCardLocation;
-  })
-  .attr("y", function (d) {
-    return d.y + 5;
+  .catch(function(error) {
+    // Do some error handling.
+    console.log("Error in data request.")
   });
 
 // Zoom
