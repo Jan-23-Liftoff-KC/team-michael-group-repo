33f3829e5c70bd7c77c50a9ec086d806176f5822
TNTull <teresatull@comcast.net>
2023-02-11 04:42:48 -0600

    validation-form 2-10-2023 working and done

--- src/main/java/org/launchcode/familytree/FamilyTreeApplication.java
+++ src/main/java/org/launchcode/familytree/FamilyTreeApplication.java
@@ -2,9 +2,7 @@ package org.launchcode.familytree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import de.codecentric.boot.admin.config.EnableAdminServer;
 
-//@EnableAdminServer
 @SpringBootApplication
 public class FamilyTreeApplication {
 
