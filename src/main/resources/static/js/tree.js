b585d65542f49322e53d6c20d3cc88c9981eea83
gabrielvalencia <gabev13@gmail.com>
2023-02-22 22:20:23 -0600

    cleanup

--- src/main/resources/static/js/tree.js
+++ src/main/resources/static/js/tree.js
@@ -37,10 +37,6 @@ let gContainer = svg.append("g").attr("transform", "translate(80,50)");
 // test-data-eleven-person.json *
 // test-data-four-person.json
 
-//console.log(document.getElementById("treeData").innerHTML);
-//let treeData = JSON.parse(document.getElementById('treeData').innerHTML);
-//let treeData = document.getElementById('treeData').innerHTML;
-
 //d3.json(
 //  "https://raw.githubusercontent.com/gvalencia4/D3/main/Family%20Tree/test-data-eleven-person.json"
 //)
