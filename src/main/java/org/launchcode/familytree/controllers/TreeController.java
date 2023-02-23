73965bf56a05b86a53bcc27b6f0024fa1e6bc4e8
gabrielvalencia <gabev13@gmail.com>
2023-02-23 08:52:28 -0600

    testing person manipulation

--- src/main/java/org/launchcode/familytree/controllers/TreeController.java
+++ src/main/java/org/launchcode/familytree/controllers/TreeController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping(value="tree")
 public class TreeController {
@@ -15,10 +18,26 @@ public class TreeController {
     @Autowired
     private PersonRepository personRepository;
 
+//    @GetMapping(path="/all")
+//    public @ResponseBody Iterable<Person> getAllPersons() {
+//        // This returns a JSON or XML with the users
+//
+//        return personRepository.findAll();
+//    }
+
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<Person> getAllPersons() {
-        // This returns a JSON or XML with the users
-        return personRepository.findAll();
+    public @ResponseBody Iterable<Person> getAllPersonsForTree() {
+        // This returns a JSON or XML with the persons
+
+        ArrayList<Person> treeJSON = new ArrayList<>();
+        //Array<Person> treeJSON = null; //???
+
+        for (Person person : personRepository.findAll()) {
+            System.out.println("Added " + person.getFirstName() + " to list.");
+            person.setFirstName(person.getFirstName() + " (edited)");
+            treeJSON.add(person);
+        }
+        return treeJSON;
     }
 }
 
