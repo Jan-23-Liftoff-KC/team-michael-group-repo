3b2b1d001d5189574ea85fa4a2a23a247f3e2736
gabrielvalencia <gabev13@gmail.com>
2023-02-24 10:05:28 -0600

    multiple roots after one is found will be ignored on the tree

--- src/main/java/org/launchcode/familytree/controllers/TreeController.java
+++ src/main/java/org/launchcode/familytree/controllers/TreeController.java
@@ -10,10 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.json.simple.JSONObject;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping(value="tree")
 public class TreeController {
@@ -26,6 +22,7 @@ public class TreeController {
     public @ResponseBody Iterable<Person> getAllPersonsForTree() {
 
         JSONArray treeJSON = new JSONArray();
+        boolean isRootFound = false;
 
         for (Person person : personRepository.findAll()) {
             JSONObject treePerson = new JSONObject();
@@ -33,7 +30,22 @@ public class TreeController {
             treePerson.put("id", person.getId());
             treePerson.put("firstName", person.getFirstName());
             treePerson.put("lastName", person.getLastName());
-            treePerson.put("parentId", person.getParentId());
+
+            // Only one root can exist in the tree (parentId = "")
+            // After one root is found, all other potential root Persons are ignored in the tree
+            if (person.getParentId() == 0) {
+                if(!isRootFound) {
+                    // Root found
+                    treePerson.put("parentId", "");
+                    isRootFound = true;
+                } else {
+                    // Ignore root, not added to treeJSON
+                    continue;
+                }
+            } else {
+                treePerson.put("parentId", person.getParentId());
+            }
+
             treePerson.put("icon", "https://github.com/Jan-23-Liftoff-KC/team-michael-group-repo/blob/main/src/main/resources/test-tree-data/person-icon.png?raw=true"); //person.getIcon());
             treePerson.put("spouse", person.getSpouseId());
 
